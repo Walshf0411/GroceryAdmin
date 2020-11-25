09cddd609e37b5068fa3d4fba8d656cba03654a3
Walsh Tony Fernandes <32847434+Walshf0411@users.noreply.github.com>
2020-11-25 15:09:28 +0530

    Update 2020_10_24_172331_create_banners_table.php

--- database/migrations/2020_10_24_172331_create_banners_table.php
+++ database/migrations/2020_10_24_172331_create_banners_table.php
@@ -14,7 +14,7 @@ class CreateBannersTable extends Migration
     public function up()
     {
         Schema::create('banners', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements("id");
             // $table->integer("banner_rank")->unique();
             $table->string("banner_image");
             $table->timestamps();
