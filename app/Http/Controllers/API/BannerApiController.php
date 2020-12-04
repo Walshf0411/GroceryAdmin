88b03a300146240924d263001b055ca0fa194798
Walsh Fernandes <walshfernades.320@gmail.com>
2020-12-04 10:59:45 +0530

    Fixed error releated to Controllers/API/BannerApiController

---
+++ app/Http/Controllers/API/BannerApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Api\ApiService;
+
+class BannerApiController
+{
+    public function __construct(ApiService $service){
+        $this->service = $service;
+    }
+
+    public function show()
+    {
+        return $this->service->getHomePage();
+    }
+
+    public function showCategory()
+    {
+        return response()->json($this->service->getAllCategory());
+
+    }
+
+}
