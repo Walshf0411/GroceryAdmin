95892706a65fd2fed546f58705bf7c5d107adcb1
Jaydeep21 <jaydeepdharamsey21@gmail.com>
2020-12-13 13:33:16 +0530

    updated jwt response

--- app/Model/Vendor.php
+++ app/Model/Vendor.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
-use Tymon\JWTAuth\Contracts\JWTSubject as ContractsJWTSubject;
-use Tymonn\JWTAuth\Contracts\JWTSubject;
-class Vendor extends Authenticatable implements ContractsJWTSubject
+use Tymon\JWTAuth\Contracts\JWTSubject;
+class Vendor extends Authenticatable implements JWTSubject
 {
     //
     use Notifiable, HasApiTokens;
@@ -22,11 +21,6 @@ class Vendor extends Authenticatable implements ContractsJWTSubject
         'password'
     ];
 
-    protected $casts = [
-
-        'created_at' => 'datetime',
-
-        ];
 
     public function getJWTIdentifier()
     {
@@ -34,6 +28,14 @@ class Vendor extends Authenticatable implements ContractsJWTSubject
     }
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+        'name'=> $this->name ,
+        'shop_name' => $this->shop_name,
+        'address' => $this->address,
+        'email_id' => $this->email_id,
+        'mobile_number'=> $this->mobile_number,
+        'gst_number'=> $this->gst_number,
+        'rating'=> $this->rating
+    ];
     }
 }
