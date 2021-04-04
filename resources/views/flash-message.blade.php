9f65d59256043d99991145f43187d5d2214e23c5
Jaydeep21 <jaydeepdharamsey21@gmail.com>
2021-04-04 15:26:43 +0530

    solving error/success message display bug

--- resources/views/flash-message.blade.php
+++ resources/views/flash-message.blade.php
@@ -1,12 +1,47 @@
+<style>
+    #hideMe {
+    -moz-animation: cssAnimation 5s ease-in 5s forwards;
+    /* Firefox */
+    -webkit-animation: cssAnimation 5s ease-in 5s forwards;
+    /* Safari and Chrome */
+    -o-animation: cssAnimation 5s ease-in 5s forwards;
+    /* Opera */
+    animation: cssAnimation 5s ease-in 5s forwards;
+    -webkit-animation-fill-mode: forwards;
+    animation-fill-mode: forwards;
+}
+/* @keyframes cssAnimation {
+    to {
+        overflow:hidden;
+    }
+}
+@-webkit-keyframes cssAnimation {
+    to {
+        visibility:hidden;
+    }
+} */
+@keyframes cssAnimation {
+    0%   {opacity: 0;display: none;position: absolute;}
+    90%  {opacity: 0;}
+    100% {opacity: 0;}
+}
+@-webkit-keyframes cssAnimation {
+    0%   {opacity: 0;display: none;position: absolute;}
+    90%  {opacity: 0;}
+    100% {opacity: 0;}
+}
+
+</style>
+
 @if ($message = Session::get('success'))
-<div class="alert alert-success alert-block">
+<div class="alert alert-success alert-block" id="hideMe">
 	<button type="button" class="close" data-dismiss="alert">×</button>
         <strong>{{ $message }}</strong>
 </div>
 
 
 @elseif ($message = Session::get('error'))
-<div class="alert alert-danger alert-block">
+<div class="alert alert-danger alert-block" id="hideMe">
 	<button type="button" class="close" data-dismiss="alert">×</button>
         <strong>{{ $message }}</strong>
 </div>
@@ -14,7 +49,7 @@
 
 
 @elseif ($message = Session::get('warning'))
-<div class="alert alert-warning alert-block">
+<div class="alert alert-warning alert-block" id="hideMe">
 	<button type="button" class="close" data-dismiss="alert">×</button>
 	<strong>{{ $message }}</strong>
 </div>
@@ -22,7 +57,7 @@
 
 
 @elseif ($message = Session::get('info'))
-<div class="alert alert-info alert-block">
+<div class="alert alert-info alert-block" id="hideMe">
 	<button type="button" class="close" data-dismiss="alert">×</button>
 	<strong>{{ $message }}</strong>
 </div>
@@ -30,7 +65,7 @@
 
 
 @elseif($errors->any())
-<div class="alert alert-danger">
+<div class="alert alert-danger" id="hideMe">
 	<button type="button" class="close" data-dismiss="alert">×</button>
 	Please check the form below for errors
 </div>
