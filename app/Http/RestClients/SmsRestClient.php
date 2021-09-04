5b7ed9812221de15c814a72998cadb2261a6616b
Walshf0411 <walshfernades.320@gmail.com>
2021-09-04 05:38:34 +0000

    Removed sms endpoint from env

--- app/Http/RestClients/SmsRestClient.php
+++ app/Http/RestClients/SmsRestClient.php
@@ -13,30 +13,35 @@ class SmsRestClient {
     }
     
     public function sendSms(String $mobileNumber, String $message) {
-        Log::info("Sending sms to contact: " . $mobileNumber . " with content: " . $message);
+        try {
+            Log::info("Sending sms to contact: " . $mobileNumber . " with content: " . $message);
+            
+            $startTime = microtime(true); 
+
+            $smsEndPoint = "http://jskbulksms.in/app/smsapi/index.php?key=36107BAE7E6D41&campaign=1&routeid=48&type=text&contacts=".$mobileNumber."&senderid=SOCSDP&msg=".$message;
+
+            $response = $this->client->request('GET', $smsEndPoint,[ 
+                'on_stats' => function (\GuzzleHttp\TransferStats $stats) use (&$url) {
+                    $url = $stats->getEffectiveUri();
+                    Log::info("Url used to send sms: " . $url);
+                }
+            ]);
+
+            $endTime = microtime(true); 
+            $execution_time = ($endTime - $startTime);
+            Log::info("Sms api took " . $execution_time . " seconds to execute");
+
+            $responseCode = $response->getStatusCode();
+            $responseBody = $response->getBody();
+
+            Log::info("Sms api returned response code: " . $responseCode);
+            Log::info("Sms api returned response body: " . $responseBody);
+            
+            return $response->getStatusCode() < 400;
+        } catch(Exception $e) {
+            Log::error("Failed to call sms endpoint".$e);
+            return false;
+        }
         
-        $startTime = microtime(true); 
-
-        $endPoint = str_replace("contacts", "contacts=".$mobileNumber, env('SMS_API_ENDPOINT'));
-        $endPoint = str_replace("msg", "msg=".$message, $endPoint);
-
-        $response = $this->client->request('GET', $endPoint,[ 
-            'on_stats' => function (\GuzzleHttp\TransferStats $stats) use (&$url) {
-                $url = $stats->getEffectiveUri();
-                Log::info("Url used to send sms: " . $url);
-            }
-        ]);
-
-        $endTime = microtime(true); 
-        $execution_time = ($endTime - $startTime);
-        Log::info("Sms api took " . $execution_time . " seconds to execute");
-
-        $responseCode = $response->getStatusCode();
-        $responseBody = $response->getBody();
-
-        Log::info("Sms api returned response code: " . $responseCode);
-        Log::info("Sms api returned response body: " . $responseBody);
-        
-        return $response->getStatusCode() <= 200;
     }
 }
